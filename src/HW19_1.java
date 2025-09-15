512d6d08d25b8b74d9a3ab07e9bd9a5795b9399d
MrOcelotGuy <dieartmen@gmail.com>
2025-09-15 15:36:17 -0700

    homework

---
+++ src/HW19_1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class HW19_1 {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(14);
+    list.add(42);
+    list.add(25);
+    
+    ArrayList<Integer> newList = removeDuplicates(list);
+    
+    System.out.print(newList);
+  }
+}
---
+++ src/HW19_2.java
@@ -0,0 +1,8 @@
+public class HW19_2 {
+  public static void main(String[] args) {
+    Integer[][] numbers = { {1, 2, 3}, {4, 4, 6}
+    };
+    System.out.println(max(numbers));
+  }
+
+}
