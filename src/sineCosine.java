2496b4054849ffe87b8724514c994855464a1969
MrOcelotGuy <dieartmen@gmail.com>
2025-09-15 16:29:50 -0700

    edited description and began sineCosine

---
+++ src/sineCosine.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Diego Arturo Meneses Prieto
+ * Date: 2025-09-09
+ * Course: CIST-004B1 - Advanced Java Data Structures
+ * Homework: #4
+ * Description: The program uses a lambda expression and an interface in order to calculate factorials
+ * Honor Code: I affirm that this program is entirely my own work.
+ * Collaboration:
+ */
+
+public class sineCosine {
+}
