514f38d4e8e47796bb299a378dfc1b4b2ba28f12
MrOcelotGuy <dieartmen@gmail.com>
2025-09-23 17:32:25 -0700

    implemented moosick

--- src/mooSick.java
+++ src/mooSick.java
@@ -10,24 +10,64 @@ public class mooSick
         int n = in.nextInt();
         for(int i = 0; i < n; i++)
         {
-            notes.addLast(in.nextInt());
+            notes.add(in.nextInt());
         }
         int c = in.nextInt();
-        SortedSet<Integer> chord = new TreeSet<Integer>();
+        ArrayList<Integer> chord = new ArrayList<>();
         for(int i = 0; i < c; i++)
         {
             chord.add(in.nextInt());
         }
+        chord.sort(null);
 
-        SortedSet<Integer> chord2 = new TreeSet<>();
+        ArrayList<Integer> chord2 = new ArrayList<>();
 
-        for(int i = 0; i < notes.size(); i++)
+        ArrayList<Integer> problemPlaces = new ArrayList<>();
+
+        boolean isEqual = false;
+
+        if(c > 1)
         {
-
-            chord.equals(chord2);
+            for (int i = 0; i <= notes.size() - c; i++) {
+                for (int j = i; j < c + i; j++) {
+                    chord2.add(notes.get(j));
+                }
+                chord2.sort(null);
+                for (int k = 1; k < c; k++) {
+                    if ((chord.get(k) - chord.get(k - 1)) != (chord2.get(k) - chord2.get(k - 1))) {
+                        isEqual = false;
+                        break;
+                    } else {
+                        isEqual = true;
+                    }
+                }
+                if (isEqual) {
+                    problemPlaces.add(i + 1);
+                }
+                chord2.clear();
+            }
+        }
+        else
+        {
+            for(int i = 0; i < notes.size(); i++)
+            {
+                problemPlaces.add(i+1);
+            }
         }
 
-        System.out.println(chord);
+
+        if(problemPlaces.isEmpty())
+        {
+            System.out.println(0);
+        }
+        else
+        {
+            System.out.println(problemPlaces.size());
+            for (Integer problemPlace : problemPlaces)
+            {
+                System.out.println(problemPlace);
+            }
+        }
 
 
     }
