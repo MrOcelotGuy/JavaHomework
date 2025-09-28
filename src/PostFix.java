c2c84f1a6509ddbda4cc0dd5270f9b334db64cec
MrOcelotGuy <dieartmen@gmail.com>
2025-09-27 19:09:47 -0700

    added postfix

--- src/PostFix.java
+++ src/PostFix.java
@@ -1,9 +1,9 @@
 import java.util.*;
 /*
  * Author: Diego Arturo Meneses Prieto
- * Date: 2025-09-12
+ * Date: 2025-09-27
  * Course: CIST-004B1 - Advanced Java Data Structures
- * Homework: #6
+ * Homework: #7
  * Description: The program uses a stack to evaluate postfix notation problems
  * Honor Code: I affirm that this program is entirely my own work.
  * Collaboration: Mr Liu you helped me understand how to do this and how to use the stack properly and how postfix notation works
@@ -18,6 +18,52 @@ public class PostFix
 
         System.out.println("Enter your postfix notation equation");
         String equation = in.nextLine();
-        
+
+        Stack<Integer> operandStack = new Stack<>();
+        String[] arrayified = equation.split(" ");
+
+        int operand1;
+        int operand2;
+
+        for(int i = 0; i < arrayified.length; i++)
+        {
+            if(arrayified[i].equals("+"))
+            {
+                operand1 = operandStack.pop();
+                operand2 = operandStack.pop();
+                operandStack.push(operand2 + operand1);
+            }
+            else if(arrayified[i].equals("-"))
+            {
+                operand1 = operandStack.pop();
+                operand2 = operandStack.pop();
+                operandStack.push(operand2 - operand1);
+            }
+            else if(arrayified[i].equals("*"))
+            {
+                operand1 = operandStack.pop();
+                operand2 = operandStack.pop();
+                operandStack.push(operand2 * operand1);
+            }
+            else if(arrayified[i].equals("/"))
+            {
+                operand1 = operandStack.pop();
+                operand2 = operandStack.pop();
+                operandStack.push(operand2 / operand1);
+            }
+            else
+            {
+                operandStack.push(Integer.valueOf(arrayified[i]));
+            }
+
+        }
+        System.out.println(operandStack);
     }
 }
+/*
+Enter your postfix notation equation
+10 5 * 10 200 40 / * + 500 -
+[-400]
+
+Process finished with exit code 0
+ */
